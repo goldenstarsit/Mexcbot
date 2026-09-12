1da3c6e1c5965ab591b01d7cd58ec75bd4cbcf49
goldenstarsit <goldenstarsit@gmail.com>
2026-09-12 14:58:26 +0500

    feat: add DCA calculator

---
+++ src/services/dcaCalculator.js
@@ -0,0 +1,32 @@
+export default class DcaCalculator {
+  constructor(tradingConfig) {
+    this.tradingConfig = tradingConfig;
+  }
+
+  calculateLevels(initialPrice) {
+    if (!Number.isFinite(initialPrice) || initialPrice <= 0) {
+      throw new Error("Initial price must be greater than 0");
+    }
+
+    const levels = this.tradingConfig.dca.levels;
+    const result = [];
+
+    for (let orderNumber = 1; orderNumber <= levels; orderNumber += 1) {
+      const dropPercent = (orderNumber * (orderNumber + 1)) / 2;
+      const targetPrice = initialPrice * (1 - dropPercent / 100);
+
+      if (targetPrice <= 0) {
+        throw new Error(`Invalid DCA target price at level ${orderNumber}`);
+      }
+
+      result.push({
+        orderNumber: orderNumber + 1,
+        dcaLevel: orderNumber,
+        dropPercent,
+        targetPrice,
+      });
+    }
+
+    return result;
+  }
+}
